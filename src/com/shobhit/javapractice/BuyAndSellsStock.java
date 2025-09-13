521741ec277c3c34e32ee8e0b61dd6266c43cece
shobhit123 <shobhittiwari859@gmail.com>
2025-09-13 21:58:40 +0530

    BuyAndSellsStock..
    second code

--- src/com/shobhit/javapractice/BuyAndSellsStock.java
+++ src/com/shobhit/javapractice/BuyAndSellsStock.java
@@ -1,5 +1,25 @@
 package com.shobhit.javapractice;
 
 public class BuyAndSellsStock {
+//find max profit
+	public static int maxProfit(int arr[]) {
+		int maxprofit = 0;
+		int minPrice = Integer.MAX_VALUE;
+		for (int price : arr) {
+			if (price < minPrice) {
+				minPrice = price;
+			} else if (price - minPrice > maxprofit) {
+				maxprofit = price - minPrice;
+			}
+		}
+		return maxprofit;
+
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = {1,5,1,2,4,5};
+		System.out.println(maxProfit(arr));
+	}
 
 }
