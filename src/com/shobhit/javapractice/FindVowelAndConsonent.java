5eabf301daf7887b6fd9c2b348e1074972a9cb10
shobhit123 <shobhittiwari859@gmail.com>
2025-09-21 10:44:53 +0530

    chnge

--- src/com/shobhit/javapractice/FindVowelAndConsonent.java
+++ src/com/shobhit/javapractice/FindVowelAndConsonent.java
@@ -2,4 +2,27 @@ package com.shobhit.javapractice;
 
 public class FindVowelAndConsonent {
 
+	public static void main(String[] args) {
+		String s = "sjsksks";
+		FindVobleAndConso(s);
+	}
+
+	public static void FindVobleAndConso(String s) {
+
+		int vovleCount = 0;
+		int consoCont = 0;
+
+		for (char ch : s.toCharArray()) {
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				vovleCount++;
+
+			} else {
+				consoCont++;
+
+			}
+		}
+		System.out.println(vovleCount + " = vowel count");
+		System.out.println(consoCont + " = consonent count");
+
+	}
 }
